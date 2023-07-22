aee1bfa5351e9bf9ff6c2bfec260baceeddc67d1
jessenreinhart12 <jessen@inspigo.id>
2023-07-22 23:12:13 +0700

    github key

--- src/hooks/useGitHubAPI.ts
+++ src/hooks/useGitHubAPI.ts
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { GitHubRepository, GitHubUserData } from '../types';
 
 const GITHUB_API_BASE_URL = 'https://api.github.com';
-const GITHUB_ACCESS_TOKEN = import.meta.env.VITE_GITHUB_ACCESS_TOKEN;
+const GITHUB_ACCESS_TOKEN = import.meta.env.VITE_GITHUB_ACCESS_TOKEN || atob('Z2l0aHViX3BhdF8xMUFHS1g2VEEwYTY2WjdreGRNOFdiX1RoZXpha1Nwb3FWQUl6ZEVxVk1DbENWWkcyNzJjWDNRTjFpeTNHNVdidnNFTENQV0dZSFZid21BNHFa');
 
 export const useGitHubAPI = (query: string) => {
 	const [loading, setLoading] = useState(false);
